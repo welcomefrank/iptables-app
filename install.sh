d8fd4f9e17faf731667537250fd429cc56dc2c6b
010ada <40416562+010ada@users.noreply.github.com>
2021-03-14 20:56:52 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ cd $DIR
 
 which python3 >/dev/null || echo "please install python3 at first!" ; exit 1
 
-python3 install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 source install.rclocal.sh
 source install.crontab.sh
